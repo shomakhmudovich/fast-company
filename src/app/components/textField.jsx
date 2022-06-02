e65e2d51e320b296e559f2aecc7ebaa014b4b567
unknown <C:\Users\admin\AppData\Roaming\The Bat!>
2022-06-02 17:22:03 +0300

    up to 8

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TextField = ({ label, type, name, value, onChange }) => {
+const TextField = ({ label, type, name, value, onChange, error }) => {
     return (
         <div>
             <label htmlFor={name}>{label}</label>
@@ -12,15 +12,20 @@ const TextField = ({ label, type, name, value, onChange }) => {
                 value={value}
                 onChange={onChange}
             />
+            {error && <p>{error}</p>}
         </div>
     );
+};
+TextField.defaultProps = {
+    type: "text"
 };
 TextField.propTypes = {
     label: PropTypes.string,
     type: PropTypes.string,
     name: PropTypes.string,
     value: PropTypes.string,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    error: PropTypes.string
 };
 
 export default TextField;
